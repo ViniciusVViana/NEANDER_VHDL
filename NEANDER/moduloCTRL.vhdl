2e771850e8344ec09589b720424f814819cae23c
Guilherme Augusto Deitos Alves <65348931+GuilhermeDeitos@users.noreply.github.com>
2023-07-24 21:24:06 -0300

    a

--- NEANDER/moduloCTRL.vhdl
+++ NEANDER/moduloCTRL.vhdl
@@ -235,17 +235,18 @@ end entity;
 
 architecture arch of decode is
 begin
-    instr_out_decode <= "10000000000" when instr_in_decode = "00000000" else (others  => 'Z');
-    instr_out_decode <= "01000000000" when instr_in_decode = "00010000" else (others  => 'Z');
-    instr_out_decode <= "00100000000" when instr_in_decode = "00100000" else (others  => 'Z');    
-    instr_out_decode <= "00010000000" when instr_in_decode = "00110000" else (others  => 'Z');    
-    instr_out_decode <= "00001000000" when instr_in_decode = "01000000" else (others  => 'Z');
-    instr_out_decode <= "00000100000" when instr_in_decode = "01010000" else (others  => 'Z');
-    instr_out_decode <= "00000010000" when instr_in_decode = "01100000" else (others  => 'Z');    
-    instr_out_decode <= "00000001000" when instr_in_decode = "10000000" else (others  => 'Z'); 
-    instr_out_decode <= "00000000100" when instr_in_decode = "10010000" else (others  => 'Z');
-    instr_out_decode <= "00000000010" when instr_in_decode = "10100000" else (others  => 'Z');    
-    instr_out_decode <= "00000000001" when instr_in_decode = "11110000" else (others  => 'Z'); 
+    instr_out_decode <= "10000000000" when instr_in_decode="00000000" else 
+              "01000000000" when instr_in_decode="00010000" else 
+              "00100000000" when instr_in_decode="00100000" else 
+              "00010000000" when instr_in_decode="00110000" else 
+              "00001000000" when instr_in_decode="01000000" else 
+              "00000100000" when instr_in_decode="01010000" else 
+              "00000010000" when instr_in_decode="01100000" else 
+              "00000001000" when instr_in_decode="10000000" else 
+              "00000000100" when instr_in_decode="10010000" else 
+              "00000000010" when instr_in_decode="10100000" else 
+              "00000000001" when instr_in_decode="11110000" else
+                (others => 'Z'); 
 end arch ; -- arch
 
 --NOP
